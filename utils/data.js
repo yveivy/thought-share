b015b12421b4d19633a1f54b56331d32bd791e0b
Valais Girl <Yevette@Hal-71.local>
2023-06-03 11:26:13 -0500

    create starter users and thoughts for database

---
+++ utils/data.js
@@ -0,0 +1,64 @@
+const users = [
+  {
+    username: "ameliabedilia",
+    email: "imtooliteral@gmail.com",
+  },
+
+  {
+    username: "harrietwelsch",
+    email: "ispy@gmail.com",
+  
+  },
+
+  {
+    username: "nancydrew"
+    email: "crackthecase@gmail.com"
+
+
+  },
+
+];
+
+const thoughts = [
+ 
+  {
+    thoughtText: "Now I must dress the chicken. I wonder if she wants a he chicken or a she chicken?",
+    createdAt: "01/01/63",
+    username: "ameliabedilia",
+
+  },
+ 
+  {
+    thoughtText: "I'm glad I'm not perfect- I'd be bored to death.",
+    createdAt: "10/01/64",
+    username: "harrietwelsch",
+
+  },
+
+  {
+    thoughtText: "Don't mess with anybody on a Monday. It's a bad, bad day.",
+    createdAt: "10/27/65",
+    username: "harrietwelsch",
+
+  },
+
+   
+  {
+    thoughtText: "If a guys hunch results in a dead-end, don't flaunt your better judgement and intuition in front of him. Smirk secretly to yourself",
+    createdAt: "04/28/30",
+    username: "dancydrew",
+
+  },
+
+  {
+    thoughtText: "I don't promise to forget the mystery, but I know I'll have a marvelous time.",
+    createdAt: "12/25/45",
+    username: "dancydrew",
+
+  },
+
+
+];
+
+// Export the functions for use in seed.js
+module.exports = { users, thoughts };
---
+++ utils/seed.js
@@ -0,0 +1,27 @@
+const connection = require('../config/connection');
+const { User, Thought } = require('../models');
+const { users, thoughts } = require('./data');
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+  console.log('connected');
+
+  // Drop existing courses
+  await User.deleteMany({});
+
+  // Drop existing students
+  await Thought.deleteMany({});
+
+
+  // Add users to the collection and await the results
+  await User.collection.insertMany(users);
+
+  // Add thoughts to the collection and await the results
+  await Thought.collection.insertMany(thoughts);
+
+  // Log out the seed data to indicate what should appear in the database
+  console.table(users);
+  console.info('Seeding complete! 🌱');
+  process.exit(0);
+});
