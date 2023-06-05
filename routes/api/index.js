09888f2cb1a9b6c29ff8fa6046c1f6649ec74b56
Valais Girl <Yevette@Hal-71.local>
2023-06-04 22:05:18 -0500

    no updates

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const thoughtRoutes = require('./thoughtRoutes');
+const userRoutes = require('./user');
+const thoughtRoutes = require('./thought');
 
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
