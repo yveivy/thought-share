1e1c53d1874eaf9a02d47eba1d608b90d8967c8c
Valais Girl <Yevette@Hal-71.local>
2023-06-04 22:01:25 -0500

    couldn't get .env, changed to string

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
-const data = require('./models/data');
+const { connect, connection } = require('mongoose');
 const mongoose = require("mongoose")
-const dotenv = require("dotenv")
+// const dotenv = require("dotenv")
 
 
-dotenv.config();
-process.env.MONGOLAB_URI;
+// dotenv.config();
 
-const connectionStringURI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@classactivities.fq7zpnf.mongodb.net/thoughtshare`
 
+
+// const connectionString =  
+//   process.env.MONGOLAB_URI || `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@classactivities.fq7zpnf.mongodb.net/T`;
+
+// connect(connectionString);
+
+mongoose.connect(`mongodb+srv://yveivy:Yevette1@classactivities.fq7zpnf.mongodb.net/thoughtshareDB`)
 
 
 module.exports = mongoose.connection;
